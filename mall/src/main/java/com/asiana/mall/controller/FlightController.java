79da1087f60ef329e881df5b2520caa26a8e2007
kimchh2 <kimchh2@asianaidt.com>
2023-02-22 16:37:00 +0900

    feat: flight 검색 기능 구현

---
+++ mall/src/main/java/com/asiana/mall/controller/FlightController.java
@@ -0,0 +1,32 @@
+package com.asiana.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.asiana.mall.service.FlightServiceImpl;
+import com.asiana.mall.vo.Flight;
+
+@Controller
+public class FlightController {
+	private FlightServiceImpl flightService;
+	
+	public FlightController(FlightServiceImpl flightService) {
+		this.flightService = flightService;
+	}
+
+	@GetMapping("/ShopMiniMall/flightForm")
+	public String flightForm(Flight flight) {		
+		return "/ShopMiniMall/flight";
+	}
+	
+	@GetMapping("/ShopMiniMall/flight")
+	public ModelAndView getFlightList(ModelAndView mv,  Flight flight) throws Exception {
+		System.out.println(flight);
+		
+		mv.setViewName("/ShopMiniMall/flight :: flightListFragment");
+		mv.addObject("flightList", flightService.searchFlight(flight));
+		return mv;
+	}
+	
+}
---
+++ mall/src/main/resources/static/js/Flight/flight.js
@@ -0,0 +1,29 @@
+
+function flightSelectCheck() {
+	if ($(':radio[name="fligthRadioButton"]:checked').length < 1) {
+		alert("항공편을 선택해주세요.");
+	} else {
+		$.ajax({
+			url : "/ShopMiniMall/",
+			type: "get",
+			data : 
+		})
+	}
+}
+
+function getFlightList() {
+	let arrival = $("#arrival").get(0).value
+	let departure = $("#departure").get(0).value
+	let boardingDate = $("#boardingDate").get(0).value
+	$.ajax({
+		url: "/ShopMiniMall/flight", 
+		type :"get",
+		data : {
+			arrival : arrival,
+			departure :departure,
+			boardingDate: boardingDate
+		}
+	}).done(function (result) {
+		$("#flightList").replaceWith(result)
+	});
+}

d8ecec83864574c98fcd8f6b28cb9cd30d9c1eb5
kimchh2 <kimchh2@asianaidt.com>
2023-02-24 10:43:25 +0900

    fix : Cart 에 담기 시큐리티 에러

--- mall/src/main/resources/static/js/ProductPage/cart.js
+++ mall/src/main/resources/static/js/ProductPage/cart.js
@@ -7,6 +7,9 @@ function createCartItem(cart) {
         totalPrice: cart["price"] * amount,            
         amount : amount 
     })
+    const token = $("meta[name='_csrf']").attr("content")
+	const header = $("meta[name='_csrf_header']").attr("content");
+
     $.ajax({
 		type :"post",
 		url: "/ShopMiniMall/cart", 
@@ -15,7 +18,9 @@ function createCartItem(cart) {
             ...cart,
             totalPrice: cart["price"] * amount,            
             amount : amount 
-		}, dataType : 'text'
+		}, dataType : 'text',beforeSend : function(xhr) {
+			xhr.setRequestHeader(header, token);
+		}
 	}).done(function (result) {
         alert("장바구니에 추가되었습니다.")
 	});
